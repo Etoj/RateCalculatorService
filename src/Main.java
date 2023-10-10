8ef6deeefbaf43c550c60be9a9b3786f461df082
Etoj <87bartek87@gmail.com>
2023-10-10 18:40:20 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import model.InputData;
+import service.*;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        InputData inputData = new InputData()
+            .withAmount(new BigDecimal(99000))
+                .withMonthsDuration(BigDecimal.valueOf(160));
+
+        PrintingService printingService = new PrintingServiceImp();
+        RateCalculationService rateCalculationService = new RateCalculationServiceImpl();
+        MortgageCalculationService mortgageCalculationService = new MortgageCalculationServiceImpl(printingService, rateCalculationService);
+        mortgageCalculationService.calculate(inputData);
+    }
+}
---
+++ src/service/RateCalculationServiceImpl.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.InputData;
+import model.Rate;
+
+import java.util.List;
+
+public class RateCalculationServiceImpl implements RateCalculationService {
+    @Override
+    public List<Rate> calculate(InputData inputData) {
+        return null;
+    }
+}
