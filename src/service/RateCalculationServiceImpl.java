bdc6d21bd7fde56cf58f566e6c03bc388680b22f
Etoj <87bartek87@gmail.com>
2023-10-16 21:09:20 +0200

    Create amount calculation service

--- src/service/RateCalculationServiceImpl.java
+++ src/service/RateCalculationServiceImpl.java
@@ -40,15 +40,15 @@ public class RateCalculationServiceImpl implements RateCalculationService {
 
     private Rate caluclateRate(BigDecimal rateNumber, InputData inputData) {
         TimePoint timePoint= timePointService.calculate(rateNumber,inputData);
-        RateAmounts rateAmounts=amountsCalculationService.calculate();
-        MortgageResidual mortgageResidual = residualCalculationService.calculate();
+        RateAmounts rateAmounts=amountsCalculationService.calculate(inputData);
+        MortgageResidual mortgageResidual = residualCalculationService.calculate(rateAmounts,inputData);
         return new Rate(rateNumber,timePoint,rateAmounts,mortgageResidual);
     }
 
     private Rate calcualteRate(BigDecimal rateNumber, InputData inputData, Rate previousRate) {
         TimePoint timePoint= timePointService.calculate(rateNumber, inputData);
-        RateAmounts rateAmounts=amountsCalculationService.calculate();
-        MortgageResidual mortgageResidual = residualCalculationService.calculate();
+        RateAmounts rateAmounts=amountsCalculationService.calculate(inputData,previousRate);
+        MortgageResidual mortgageResidual = residualCalculationService.calculate(rateAmounts,previousRate);
 
         return new Rate(rateNumber,timePoint,rateAmounts,mortgageResidual);
     }
